be84719901577795de806e0c3acc325872dfbb1b
timoast <timstuart90@gmail.com>
2017-10-03 16:59:13 +0800

    colors

--- R/plotting.R
+++ R/plotting.R
@@ -12,11 +12,12 @@
 #' 
 #' @export
 #' 
-#' @example
+#' @examples
 #' mat <- matrix(seq(100)) 
 #' hmap(mat, title = "Example heatmap")
 #' 
-hmap <- function(mat, col = "RdYlBu", rotate = TRUE, reverse_color = TRUE, title = "", ...){
+hmap <- function(mat, col = "RdYlBu", rotate = TRUE,
+                 reverse_color = TRUE, title = "", ...){
     if(rotate) {
         m <- t(apply(mat, 2, rev))
     } else {
@@ -38,7 +39,8 @@ hmap <- function(mat, col = "RdYlBu", rotate = TRUE, reverse_color = TRUE, title
 #' 
 #' @export
 #' 
-#' @example 
+#' @examples
+#' library(stuart)
 #' define_fonts()
 #' 
 set_fonts <- function (set = TRUE) {
@@ -51,4 +53,34 @@ set_fonts <- function (set = TRUE) {
                               "Helvetica Neue Light Italic", 
                               "Helvetica Neue Bold Italic"))
     if(set){ par(family = 'helvetica') }
+}
+
+#' Colour schemes
+#' 
+#' Vectors of different colours for use in figures
+#' @param type Name of colour scheme to choose colours from
+#' @return Vector of character strings of colours in hexadecimal
+#' 
+#' @export
+#' 
+stuart_colors <- function(type = 'three') {
+    # categorical
+    pairs <- c("#F98866", "#FF420E", "#80BD9E", "#89DA59")
+    four <- c("#375E97", "#FB6542", "#FFBB00", "#3F681C")
+    genotypes <- c("#DAECF3", "#FF404E","#1CA5B8", "#D3D3D3")
+    three <- c("#225378", "#ACF0F2", "#EB7F00")
+    mc <- c('#B4B464', '#6665AD', '#B29492')
+    flat <- c("#9b59b6", "#3498db", "#e74c3c", "#34495e", "#2ecc71")
+    
+    # continuous
+    darkblue <- c("#121C25", "#13314D", "#184169", "#2D587B", "#9CB1BF")
+    
+    switch(type,
+           pairs=pairs,
+           four=four,
+           genotypes=genotypes,
+           three=three,
+           mc=mc,
+           flat=flat,
+           darkblue=darkblue)
 }
